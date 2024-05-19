9b49b1b38ed24d2e32b508fd5f395e6f04f87e2e
Ayush Raj <119657235+rajayush01@users.noreply.github.com>
2024-05-19 16:17:35 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",//2rem
+      screens: {
+        //"2xl": "1400px",
+        sm:"320px",
+        md:"640px",
+        lg:"768px",
+        xl:"1200px",
+      },
+      fontFamily: {
+        primary: "var(--font-jetbrainsMono)",
+      }
+      
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
